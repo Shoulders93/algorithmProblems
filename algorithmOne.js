2c935f074375e0c195f0a84f05a286901c8c04fa
Kory Attleson <kory.attleson@gmail.com>
2021-08-02 15:05:32 -0500

    Redid algorithm one in order to understand .length - 1 better

--- algorithmOne.js
+++ algorithmOne.js
@@ -1,10 +1,26 @@
-function reverseAString(word){
-    let reverseWord = ""
-    for(let i = word.length - 1; i >= 0; i--){
-        reverseWord += word[i];
+// function reverseAString(word){
+//     let reverseWord = ""
+//     for(let i = word.length - 1; i >= 0; i--){
+//         reverseWord += word[i];
+//     }
+//     return reverseWord;
+// }
+
+// let finalReversedWord = reverseAString("Hello");
+// console.log(finalReversedWord);
+
+
+function reverseThisString(string){
+    let reverseWord = "";
+    for(let i = string.length - 1; i >= 0; i--){
+        reverseWord += string[i];
     }
     return reverseWord;
 }
+let finalWord = reverseThisString(prompt("Pick a random word"));
+console.log(finalWord);
 
-let finalReversedWord = reverseAString("Hello");
-console.log(finalReversedWord);
+// Hello, [0 1 2 3 4]
+// let word = "Hello";
+// let reverseWord = word[word.length - 1];
+// console.log(reverseWord);

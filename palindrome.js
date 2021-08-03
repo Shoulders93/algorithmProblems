0553afabec76716a6bceda677d7370f2fd9c3dd5
Kory Attleson <kory.attleson@gmail.com>
2021-08-03 10:22:17 -0500

    Redid palindrome since I have a better understanding of how to solve it

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,19 @@
 // create a function to determine if a string is a Palindrome
-// let word = "Hello World";
-// console.log(word);
+
+// palindrome is a word, or phrase, that reads the same backwards as it does forward (example; madam)
+// need to write code that takes a users input to determine if it is a palindrome
+// need to report the result;
 
 
-function isThisAPalindrome(word){
-    for(let i = 0; i < word.length; i++){
-        console.log(word[i]);
-        console.log(word[word.length- i - 1]);
-        if(word[i] !== word[word.length - i - 1]){
-            return false;
+function determineIfPalindrome(word){
+    for(i = 0; i < word.length / 2; i++){ // added / 2 so entire word does not print out in console;
+        console.log(word[i]); // print entire string so user can see the word they inputted
+        if(word[i] !== word[word.length -1 -i]){ // If first letter is not equal to last letter
+            return "This is not a Palindrome";
         }
     }
-    return true;
+    return "This is a Palindrome";
 }
+let endValue = determineIfPalindrome(prompt("Please enter a word"));
+console.log(endValue);
 
-console.log(isThisAPalindrome(prompt("Please enter a word")));
